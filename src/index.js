6f66adee9b525458ab22e3c8f065d4170488e340
Kozazavr <sohee@yandex.ru>
2021-02-06 23:34:54 +0300

    complite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function toReadable (number) {
-  
-}
+   const arrNumbers = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+   const arrDozen = ['', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+   const hundred = 'hundred';
+
+   let strNumber = String(number);
+   let arrWord = [];
+
+   if( number == 0) {
+       return 'zero';
+   }
+
+   if (number < 20) {
+       return arrNumbers[number];
+   }
+
+   if (number >= 20 && number < 100) {
+      arrWord.push(arrDozen[strNumber[0] - 1]);
+      arrWord.push(arrNumbers[strNumber[1]]);  
+      return arrWord.join(' ').trim();
+   }
+
+   if (number >= 100) {
+       arrWord.push(arrNumbers[strNumber[0]]);
+       arrWord.push(hundred);
+       if ((number - strNumber[0] * 100) > 19) {
+           arrWord.push(arrDozen[strNumber[1] - 1]);
+           arrWord.push(arrNumbers[strNumber[2]]);  
+       } else if (strNumber[1] > 0) {
+          arrWord.push(arrNumbers[strNumber[1] + strNumber[2]]);
+       } else {
+          arrWord.push(arrNumbers[strNumber[2]]);
+       }
+       return arrWord.join(' ').trim();
+   }
+};
